7408e90c28e914938d81be242e2e2b213158b906
Sandeep Shivajirao <sndy.chvn.fr@gmail.com>
2018-06-30 01:30:03 +0200

    Update
    DAO package and interface creation

--- src/uim/dao/DAO.java
+++ src/uim/dao/DAO.java
@@ -1,5 +1,16 @@
 package uim.dao;
 
-public class DAO {
+import java.io.Serializable;
 
+public interface DAO {
+	
+	public void CreateOperation(T entity) throws EntityCreationException;
+
+	public void DeleteOperation(T entity) throws EntityDeletionException;
+
+	public void UpdateOperation(T entity) throws EntityUpdateException;
+
+	public Identity getById(Serializable id) throws EntityReadException;
+
+	public List<Identity> search(T criteria) throws EntitySearchException;
 }
